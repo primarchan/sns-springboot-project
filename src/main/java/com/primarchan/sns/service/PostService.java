66af87fc8c4963e70af450149b26238d2abcdcab
An T.G <boltcarrier@naver.com>
2022-10-27 04:17:34 +0900

    포스트 작성 기능 테스트 코드 작성

---
+++ src/main/java/com/primarchan/sns/service/PostService.java
@@ -0,0 +1,31 @@
+package com.primarchan.sns.service;
+
+import com.primarchan.sns.exception.ErrorCode;
+import com.primarchan.sns.exception.SnsApplicationException;
+import com.primarchan.sns.model.entity.PostEntity;
+import com.primarchan.sns.model.entity.UserEntity;
+import com.primarchan.sns.repository.PostEntityRepository;
+import com.primarchan.sns.repository.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostEntityRepository postEntityRepository;
+    private final UserEntityRepository userEntityRepository;
+
+    @Transactional
+    public void create(String title, String body, String userName) {
+        // user find
+        UserEntity userEntity = userEntityRepository.findByUserName(userName)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+        // post save
+        postEntityRepository.save(new PostEntity());
+
+        // return
+    }
+
+}
---
+++ src/test/java/com/primarchan/sns/service/PostServiceTest.java
@@ -0,0 +1,58 @@
+package com.primarchan.sns.service;
+
+import com.primarchan.sns.exception.ErrorCode;
+import com.primarchan.sns.exception.SnsApplicationException;
+import com.primarchan.sns.model.entity.PostEntity;
+import com.primarchan.sns.model.entity.UserEntity;
+import com.primarchan.sns.repository.PostEntityRepository;
+import com.primarchan.sns.repository.UserEntityRepository;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @MockBean
+    private PostEntityRepository postEntityRepository;
+
+    @MockBean
+    private UserEntityRepository userEntityRepository;
+
+    void 포스트_작성이_성공한_경우() {
+        String title = "title";
+        String body = "body";
+        String userName = "userName";
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mock(UserEntity.class)));
+        when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+
+        Assertions.assertDoesNotThrow(() -> postService.create(title, body, userName));
+    }
+
+    void 포스트_작성시_요청한_유저가_존재하지않는_경우() {
+        String title = "title";
+        String body = "body";
+        String userName = "userName";
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, () -> postService.create(title, body, userName));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, e.getErrorCode());
+    }
+
+}
